55595b9578f020401281e46b1f7c441c3ad5b15a
Soha Afsana <afsanasoha38@gmail.com>
2025-10-12 19:50:57 +0530

    first commit

--- resume-frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ResumeUpload from "./resumeUpload.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ResumeUpload />
-  </React.StrictMode>
-);
